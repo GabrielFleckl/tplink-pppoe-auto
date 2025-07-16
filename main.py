f78296c901b0cf6e3d19b19cbcc18a878e4bd506
Gabriel Gonçalves <103828336+GabrielFleckl@users.noreply.github.com>
2025-07-16 09:47:19 -0300

    feat: better logger

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import (
-    StaleElementReferenceException,
-    TimeoutException,
-    WebDriverException
-)
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import logging
+
+
+def configurar_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="|%(levelname)s| [%(asctime)s] %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+    )
 
 
 def validar_argumento(value):
@@ -21,7 +26,7 @@ def validar_argumento(value):
 
 
 def configurar_driver():
-    print("LOG: Iniciando automação...")
+    logging.info("Iniciando automação...")
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
@@ -30,48 +35,51 @@ def configurar_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--log-level=3")
     options.add_argument("--silent")
-    options.add_argument("--user-data-dir=/tmp/chrome-profile")  
+    options.add_argument("--user-data-dir=/tmp/chrome-profile")
 
     return webdriver.Chrome(options=options)
 
 
 def realizar_login(nav, wait, url, senha):
     if not url.startswith("http"):
-        print("LOG: ❌ A URL deve começar com 'http://' ou 'https://'.")
+        logging.error("❌ A URL deve começar com 'http://' ou 'https://'.")
         exit(1)
 
     try:
         nav.get(url)
     except WebDriverException:
-        print("LOG: ❌ Erro ao acessar a URL do roteador.")
+        logging.error("❌ Erro ao acessar a URL do roteador.")
         nav.quit()
         exit(1)
 
     try:
-        print("LOG: Entrou na tela de login: " + url)
+        logging.info("Entrou na tela de login: " + url)
         login_input = wait.until(EC.visibility_of_element_located((By.ID, "pc-login-password")))
         nav.execute_script("arguments[0].scrollIntoView(true);", login_input)
-        print("LOG: Preencheu a senha do roteador")
+        logging.info("Preencheu a senha do roteador")
         login_input.send_keys(senha)
 
         login_button = wait.until(EC.element_to_be_clickable((By.ID, "pc-login-btn")))
         nav.execute_script("arguments[0].scrollIntoView(true);", login_button)
         login_button.click()
-        print("LOG: Clicou no botão de login")
+        logging.info("Clicou no botão de login")
 
         try:
             confirm_btn = WebDriverWait(nav, 5).until(EC.presence_of_element_located((By.ID, "confirm-yes")))
             nav.execute_script("arguments[0].click();", confirm_btn)
-            print("LOG: Clicou no botão para confirmar o login")
+            logging.info("Clicou no botão para confirmar o login")
         except:
-            pass  
+            pass
+
+        
 
         advanced_button = wait.until(EC.element_to_be_clickable((By.ID, "advanced")))
+        logging.info("Fez login no roteador")
         nav.execute_script("arguments[0].click();", advanced_button)
-        print("LOG: Clicou no advanced")
+        logging.info("Clicou no advanced")
 
     except TimeoutException:
-        print("LOG: ❌ Erro de login. Verifique a senha ou a interface do roteador.")
+        logging.error("❌ Erro ao fazer login. Verifique a senha do roteador.")
         nav.quit()
         exit(1)
 
@@ -82,21 +90,21 @@ def navegar_para_pppoe(nav, wait):
             time.sleep(1.5)
             network_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="menuTree"]/li[2]/a')))
             nav.execute_script("arguments[0].click();", network_button)
-            print("LOG: Clicou no network")
+            logging.info("Clicou no network")
             break
         except StaleElementReferenceException:
-            print("LOG: Elemento obsoleto. Tentando novamente...")
+            logging.info("Elemento obsoleto. Tentando novamente...")
         except TimeoutException:
-            print("LOG: Elemento 'Network' não apareceu a tempo.")
+            logging.error("Elemento 'Network' não apareceu a tempo.")
             return False
 
     gpon_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="menuTree"]/li[2]/ul/li[1]/a/span')))
     nav.execute_script("arguments[0].click();", gpon_button)
-    print("LOG: Clicou no GPON WAN dentro da barra lateral")
+    logging.info("Clicou no GPON WAN dentro da barra lateral")
 
     pppoe_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="wanBody"]/tr[3]/td[7]/span[1]')))
     nav.execute_script("arguments[0].click();", pppoe_button)
-    print("LOG: Clicou na interface PPPoE para modificar")
+    logging.info("Clicou na interface PPPoE para modificar")
 
     return True
 
@@ -106,12 +114,12 @@ def alterar_pppoe_login(nav, wait, pppoe_login):
     nav.execute_script("arguments[0].scrollIntoView(true);", login_input)
     login_input.clear()
     login_input.send_keys(pppoe_login)
-    print("LOG: Preencheu o login PPPoE com: " + pppoe_login)
+    logging.info("Preencheu o login PPPoE com: " + pppoe_login)
 
     save_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="saveConnBtn"]')))
     nav.execute_script("arguments[0].scrollIntoView(true);", save_button)
     nav.execute_script("arguments[0].click();", save_button)
-    print("LOG: Clicou no botão para salvar")
+    logging.info("Clicou no botão para salvar")
 
 
 # -------------------------
@@ -128,6 +136,8 @@ URL_ROTEADOR = args.url
 SENHA_ROTEADOR = args.senha
 LOGIN_PPPOE = args.pppoe
 
+configurar_logging()
+
 nav = configurar_driver()
 wait = WebDriverWait(nav, 15)
 
@@ -135,8 +145,8 @@ realizar_login(nav, wait, URL_ROTEADOR, SENHA_ROTEADOR)
 
 if navegar_para_pppoe(nav, wait):
     alterar_pppoe_login(nav, wait, LOGIN_PPPOE)
-    print("LOG: ✅ PPPoE trocado com sucesso!")
+    logging.info("✅ PPPoE trocado com sucesso!")
 else:
-    print("LOG: ❌ Falha ao acessar a área de configuração PPPoE.")
+    logging.error("❌ Falha ao acessar a área de configuração PPPoE.")
 
 nav.quit()
